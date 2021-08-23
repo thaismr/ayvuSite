c60c6d097128755165409df5136396c2d0deb68d
Thaís Mendes Ramalho <thaismr@users.noreply.github.com>
2021-08-23 19:22:33 -0300

    Add blog post creation from user dashboard.

--- ayvuSite/env/local_settings.py
+++
@@ -1,8 +0,0 @@
-# For production, check:
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4)ggz8twvt85sce!c-mie%#9rz^#)@adk+n@9+@1v0oel&+nwl'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
